178e8fecc1ddf26685760d21abe8aa68182da659
Tigran <tigranateyan2009@gmail.com>
2025-06-01 14:32:13 +0400

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -60,3 +60,56 @@ TEST(LinkedListTest, OutOfRangeRemove) {
     EXPECT_THROW(list.Remove(3), IndexOutOfBounds);
 }
 
+TEST(LinkedListTest, CopyConstructorCreatesDeepCopy) {
+    LinkedList<int> list;
+    list.InsertTail(1);
+    list.InsertTail(2);
+
+    LinkedList<int> copy(list);
+    EXPECT_EQ(copy.Size(), 2);
+    EXPECT_EQ(copy.Get(0), 1);
+    EXPECT_EQ(copy.Get(1), 2);
+
+    list.Remove(0);
+    EXPECT_EQ(copy.Get(0), 1);  
+}
+
+TEST(LinkedListTest, MoveConstructorTransfersOwnership) {
+    LinkedList<int> list;
+    list.InsertTail(1);
+    list.InsertTail(2);
+
+    LinkedList<int> moved(std::move(list));
+    EXPECT_EQ(moved.Size(), 2);
+    EXPECT_EQ(moved.Get(0), 1);
+    EXPECT_EQ(moved.Get(1), 2);
+    EXPECT_EQ(list.Size(), 0);  
+}
+
+TEST(LinkedListTest, CopyAssignmentWorksCorrectly) {
+    LinkedList<int> list;
+    list.InsertTail(1);
+    list.InsertTail(2);
+
+    LinkedList<int> copy;
+    copy = list;
+    EXPECT_EQ(copy.Size(), 2);
+    EXPECT_EQ(copy.Get(0), 1);
+    EXPECT_EQ(copy.Get(1), 2);
+
+    list.Remove(0);
+    EXPECT_EQ(copy.Get(0), 1);  
+}
+
+TEST(LinkedListTest, MoveAssignmentTransfersOwnership) {
+    LinkedList<int> list;
+    list.InsertTail(1);
+    list.InsertTail(2);
+
+    LinkedList<int> moved;
+    moved = std::move(list);
+    EXPECT_EQ(moved.Size(), 2);
+    EXPECT_EQ(moved.Get(0), 1);
+    EXPECT_EQ(moved.Get(1), 2);
+    EXPECT_EQ(list.Size(), 0);  
+}
